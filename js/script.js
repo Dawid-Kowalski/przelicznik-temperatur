7aad32be6e445d6da6b4b97877ce24373379294f
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-11-21 10:32:38 +0100

    Make variable for Celsjus, Fahrenheit and Temperature

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function giveTemperature() {
-	alert("Podaj temperaturę");
+
+	const isCelsjus = document.getElementById("celsjus").checked;
+	const isFahrenheit = document.getElementById("fahrenheit").checked;
+	const temperature =  document.getElementById("temperature").value;
+
+	alert("w celsjuszach: " + isCelsjus + "\n" + "w fahrenheitah: " + isFahrenheit + "\n" + "temperatura: " + temperature);
 }
