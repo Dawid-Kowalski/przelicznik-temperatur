2041d1b489c989089aa6471e01191debe939fdf4
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-11-21 10:53:45 +0100

    Add convert functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-function giveTemperature() {
+function convertTemperature() {
 
 	const isCelsjus = document.getElementById("celsjus").checked;
 	const isFahrenheit = document.getElementById("fahrenheit").checked;
 	const temperature =  document.getElementById("temperature").value;
 
-	alert("w celsjuszach: " + isCelsjus + "\n" + "w fahrenheitah: " + isFahrenheit + "\n" + "temperatura: " + temperature);
+	let temperatureResult = undefined;
+
+	if(isCelsjus == true) {
+		temperatureResult = ((temperature - 32) / 1.8).toFixed(2);
+		alert("temperatura w celsjuszach: " + temperatureResult + "\n" + " temperatura w fahrenheitah: " + temperature);
+	}
+
+	if(isFahrenheit == true) {
+		temperatureResult = ((temperature * 1.8) + 32).toFixed(2);
+		alert("temperatura w celsjuszach: " + temperature + "\n" + " temperatura w fahrenheitah: " + temperatureResult);
+	}
+		
 }
